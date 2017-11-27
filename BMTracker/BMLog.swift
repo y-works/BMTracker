604d7d39b8360c02f1f2796779929365ff5b2d0e
Hiroyuki Narita <davidoff1125@gmail.com>
2017-11-27 23:50:28 +0900

    コアデータ作成中

---
+++ BMTracker/BMLog.swift
@@ -0,0 +1,30 @@
+//
+//  BMLog.swift
+//  BMTracker
+//
+//  Created by 成田裕之 on 2017/11/27.
+//  Copyright © 2017年 Hiroyuki Narita. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class BMlogAccessor {
+    
+    func create(bm :Double, date :Date) {
+       
+    }
+    
+    func read() {
+        
+    }
+    
+    func update() {
+        
+    }
+    
+    func delete() {
+        
+    }
+}
---
+++ BMTracker/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  BMTracker
+//
+//  Created by 成田裕之 on 2017/11/27.
+//  Copyright © 2017年 Hiroyuki Narita. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class PersonAccessor {
+    let entityName = "Person"
+    let attrName = "name"
+    let attrHeight = "height"
+    let attrGoal = ""
+    
+    func delete(object :Person) {
+        
+    }
+}
